db6b353b7a30669effbe192eb0432c2d456d8eb9
leon_huang <leon_huange@17life.com>
2025-11-17 12:05:46 +0800

    feature: error拋送到slack

--- ElasticDemo/Readme.md
+++ ElasticDemo/Readme.md
@@ -5,3 +5,6 @@
     # tagname: 自定義image名稱，版號可不帶預設為latest，若想帶上板號格視為 {tagname}:{version}
     ```
 2. switch to ElasticDemo and run `docker compose up -d` 
+3. https://api.slack.com/apps 
+    1. → Create New App → From scratch → 選你的 Workspace
+    2. Feature -> Incoming Webhooks -> Activate Incoming Webhooks -> 打開開關 -> Add New Webhook -> 選擇WorkSapce、Channel -> Install test
--- ElasticDemo/logServerDemo/app.py
+++ ElasticDemo/logServerDemo/app.py
@@ -2,10 +2,17 @@ import time
 import logging
 
 # 設定 logging
-logging.basicConfig(filename="logs/log.log", level=logging.INFO, format="%(asctime)s - %(message)s")
+logging.basicConfig(
+    filename="logs/log.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 a = 1
 while True:
     logging.info(f"自動記錄 Log, 第{a}筆記錄")
+    if a % 3 == 0:
+        logging.error(f"自動記錄 Error Log, 第{a}筆記錄")
     a += 1
     time.sleep(60)
